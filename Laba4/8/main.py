811880c8143c1247300130ac56addf002b4f5071
Weltburger <pakowski398@yandex.ru>
2020-03-20 17:30:54 +0200

    lab4 ex2 done + ex8 fixed

--- Laba4/8/main.py
+++ Laba4/8/main.py
@@ -1,5 +1,6 @@
 import pandas
 
+
 # "name";"tld";"cca2";"ccn3";"cca3";"cioc";"currency";
 # "callingCode";"capital";"altSpellings";"region";"subregion";
 # "languages";"translations";"latlng";"demonym";"landlocked";"borders";"area"
@@ -13,17 +14,17 @@ def print_names(names):
     for i, name in enumerate(names, 1):
         print(str(i) + '.', name.split(',')[0])
 
-data = pandas.read_csv('countries.csv', ';')
-sorted_by_area = data.sort_values(by='area', ascending=False)
 
+data = pandas.read_csv('countries.csv', ';')
+
+sorted_by_area = data.sort_values(by='area', ascending=False)
 print('The largest countries')
 print_names(sorted_by_area.head(10).name)
+
 print('\nThe smallest countries')
 print_names(reversed(list(sorted_by_area.tail(10).name)))
 
-french_speaking = data.where(pandas.Series(['French' in str(d)
-                                            for d in data.languages])
-                             ).name.dropna()
+french_speaking = data.where(pandas.Series(['French' in str(d) for d in data.languages])).name.dropna()
 print('\nFrench-speaking countries')
 print_names(french_speaking)
 
@@ -31,9 +32,7 @@ island = data.where(data.borders.isnull()).name.dropna()
 print('\nIsland countries')
 print_names(island)
 
-southern = data.where(pandas.Series([float(str(d).split(',')[0]) < 0
-                                    for d in data.latlng])
-                      ).name.dropna()
+southern = data.where(pandas.Series([float(str(d).split(',')[0]) < 0 for d in data.latlng])).name.dropna()
 print('\nCountries in the Southern Hemisphere')
 print_names(southern)
 
@@ -43,6 +42,11 @@ for i, group in data.groupby([d[0] for d in data.name]):
     print_names(group.name)
     print()
 
+print('\n\nCountries by areas')
+for i, group in data.groupby([d for d in data.area]):
+    print(str(i) + ': ')
+    print_names(group.name)
+    print()
 
 names = pandas.Series([d.split(',')[0] for d in data.name])
 names.name = 'name'
@@ -53,10 +57,6 @@ lat, lng = zip(*[d.split(',')
 lat, lng = map(pandas.Series, (lat, lng))
 lat.name = 'latitude'
 lng.name = 'longitude'
-for_export = pandas.concat([names,
-                            data[['capital', 'area', 'currency']],
-                            lat,
-                            lng],
-                           axis=1)
+for_export = pandas.concat([names, data[['capital', 'area', 'currency']], lat, lng], axis=1)
 with pandas.ExcelWriter('exported.xls') as excel_writer:
     for_export.to_excel(excel_writer)
